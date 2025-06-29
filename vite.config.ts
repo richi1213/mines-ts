42567e36862688d495aefd0aa252dadfae8b9a10
richi1213 <datonanava11@gmail.com>
2025-06-29 18:09:16 +0400

    added initial structure + aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   server: {
     port: 8080,
     open: true,
+  },
+  resolve: {
+    alias: {
+      '@core': path.resolve(__dirname, 'src/core'),
+      '@game': path.resolve(__dirname, 'src/game'),
+      '@ui': path.resolve(__dirname, 'src/ui'),
+      '@config': path.resolve(__dirname, 'src/config'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@assets': path.resolve(__dirname, 'public/assets'),
+    },
   },
 });
